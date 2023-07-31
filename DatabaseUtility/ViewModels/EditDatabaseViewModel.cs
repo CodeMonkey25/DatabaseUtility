722c2d5547d434959d6bde2012a4497ddf88251d
Jason Sarley <jsarley@lcs.com>
2023-07-31 12:42:19 -0400

    general tweaks and bug fixes

--- DatabaseUtility/ViewModels/EditDatabaseViewModel.cs
+++ DatabaseUtility/ViewModels/EditDatabaseViewModel.cs
@@ -1,4 +1,6 @@
-﻿using DatabaseUtility.Models;
+﻿using System;
+using System.Reactive.Linq;
+using DatabaseUtility.Models;
 using ReactiveUI;
 
 namespace DatabaseUtility.ViewModels;
@@ -8,16 +10,28 @@ public class EditDatabaseViewModel : ViewModelBase
     private DatabaseConnection OriginalDatabaseConnection { get; }
 
     private DatabaseConnection _workingDatabaseConnection = new();
+    private IObservable<bool> _canSaveChanges = Observable.Empty<bool>();
+    
     public DatabaseConnection WorkingDatabaseConnection
     {
         get => _workingDatabaseConnection;
         set => this.RaiseAndSetIfChanged(ref _workingDatabaseConnection, value);
+    }
+    
+    public IObservable<bool> CanSaveChanges
+    {
+        get => _canSaveChanges;
+        set => this.RaiseAndSetIfChanged(ref _canSaveChanges, value);
     }
 
     public EditDatabaseViewModel(DatabaseConnection databaseConnection)
     {
         OriginalDatabaseConnection = databaseConnection;
         WorkingDatabaseConnection.OverwriteWith(databaseConnection);
+
+        CanSaveChanges = WorkingDatabaseConnection
+            .WhenAnyValue(x => x.Name, x => x.Server, x => x.UserName, x => x.Password)
+            .Select(x => !string.IsNullOrEmpty(x.Item1) && !string.IsNullOrEmpty(x.Item2) && !string.IsNullOrEmpty(x.Item3) && !string.IsNullOrEmpty(x.Item4));
     }
 
     public void SaveChanges()
